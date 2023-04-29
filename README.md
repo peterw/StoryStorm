77dc782f9a45f2ee55f03475baf4dd042116dbab
Peter <mycodeisbad@gmail.com>
2023-04-29 13:34:01 -0400

    Create .env.example
    Create main.py
    
    Update .env.example
    
    Create .gitignore
    
    Create LICENSE.md
    
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Twitter-Demo
+# Story Storm
+We got a treat for you! Introducing Story Storm, a powerful tool that harnesses the power of OpenAI's GPT-3.5 Turbo API to generate fascinating and engaging stories.
+But we didn't stop there! We wanted to make your story experience even more immersive. That's why we've also integrated the Eleven Labs API to convert your generated story into an audio narration.
+And that's not all! We've also incorporated the replicate API to generate accompanying images for your story. So now, as you listen, you can also see vivid depictions of the story's characters and settings.
+
+## Getting Started
+To get started, clone the repository and install the required dependencies:
+
+
+```git clone https://github.com/your-username/story-storm.git```
+
+```cd Story_storm```
+
+```pip install -r requirements.txt```
+
+## Prerequisites
+You will need to set up API keys for[OpenAI](https://platform.openai.com/account/api-keys),[Eleven Labs ](https://beta.elevenlabs.io/speech-synthesis) [Replicate](https://replicate.com/account/api-tokens). 
+
+### Windows
+```setx OPENAI_API_KEY <your_openai_api_key>```
+
+```setx ELEVEN_API_KEY <your_eleven_labs_api_key>```
+
+```setx REPLICATE_API_KEY <your_replicate_api_key>```
+### Linux and Mac
+```export OPENAI_API_KEY=<your_openai_api_key>```
+
+```export ELEVEN_API_KEY=<your_eleven_labs_api_key>```
+
+```export REPLICATE_API_KEY=<your_replicate_api_key>```
+
+## Usage
+To run the program, simply run the following command in the terminal:
+
+```streamlit run main.py --server.port 8080```
+
+Then enter a word and select a voice to generate a story.
+
+## Credits
+This project uses the following libraries:
+
+1. langchain
+2. streamlit
+3. dotenv
+4. elevenlabs
+5. replicate
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ main.py
@@ -0,0 +1,82 @@
+
+import os
+
+import replicate
+import streamlit as st
+from dotenv import load_dotenv
+from elevenlabs import generate
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+# Set your API keys
+openai_api_key = os.getenv("OPENAI_API_KEY")
+eleven_api_key = os.getenv("ELEVEN_API_KEY")
+
+# Initialize OpenAI model
+llm = OpenAI(temperature=0.9)
+
+
+def generate_story(text):
+    """Generate a story using the langchain library and OpenAI's GPT-3 model."""
+    prompt = PromptTemplate(
+        input_variables=["text"],
+        template=""" 
+         You are a fun and seasoned storyteller. Generate a story for me about {text}.
+                 """
+    )
+    story = LLMChain(llm=llm, prompt=prompt)
+    return story.run(text=text)
+
+
+def generate_audio(text, voice):
+    """Convert the generated story to audio using the Eleven Labs API."""
+    audio = generate(text=text, voice=voice, api_key=eleven_api_key)
+    return audio
+
+
+def generate_images(story_text):
+    """Generate images using the story text using the Replicate API."""
+    output = replicate.run(
+        "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+        input={"prompt": story_text}
+    )
+    return output
+
+
+# Streamlit app
+def app():
+    st.title("Story Storm")
+
+    # Form inputs
+    with st.form(key='my_form'):
+        text = st.text_input(
+            "Enter a word to generate a story",
+            max_chars=None,
+            type="default",
+            placeholder="Enter a word to generate a story",
+        )
+        options = ["Bella", "Antoni", "Arnold", "Adam", "Domi", "Elli", "Josh", "Rachel", "Sam"]
+        voice = st.selectbox("Select a voice", options)
+
+        if st.form_submit_button("Submit"):
+            # Show a spinner
+            with st.spinner('Generating story...'):
+                # Generate story and audio
+                story_text = generate_story(text)
+                audio = generate_audio(story_text, voice)
+
+            # Display audio and images
+            st.audio(audio, format='audio/mp3')
+            images = generate_images(story_text)
+            for item in images:
+                st.image(item)
+
+    if not text or not voice:
+        st.info("Please enter a word and select a voice")
+
+
+if __name__ == '__main__':
+    app()
